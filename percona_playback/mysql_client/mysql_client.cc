8d91bc658334a03af2a53a94cd3e5a5f2975e573
Stewart Smith <stewart@flamingspork.com>
2011-11-17 17:27:17 +1100

    add missing license to mysql_client

--- percona_playback/mysql_client/mysql_client.cc
+++ percona_playback/mysql_client/mysql_client.cc
@@ -1,4 +1,17 @@
-
+/* BEGIN LICENSE
+ * Copyright (C) 2011 Percona Inc.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * END LICENSE */
 
 #include <percona_playback/mysql_client/mysql_client.h>
 
