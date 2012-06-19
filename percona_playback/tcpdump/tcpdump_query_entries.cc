0914982da67ae7d9cb0150e56d4dbf0f6f0a5d1e
Vlad Lesin <vlad_lesin@mail.ru>
2012-06-19 19:26:53 +0400

    1) Tcpdump plugin functionality is added. 2) Tests for tcpdump plugin accuracy mode, multiple connections,    connections without handshake, fragmented packets are added.

---
+++ percona_playback/tcpdump/tcpdump_query_entries.cc
@@ -0,0 +1,129 @@
+/* BEGIN LICENSE
+ * Copyright (C) 2011-2012 Percona Inc.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * END LICENSE */
+#include "tcpdump_query_entries.h"
+#include "tcpdump.h"
+#include "percona_playback/db_thread.h"
+#include "percona_playback/plugin.h"
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/foreach.hpp>
+#include <boost/shared_ptr.hpp>
+#include <string.h>
+#include <iostream>
+#include <assert.h>
+
+void
+TcpdumpQueryEntry::execute(DBThread *t)
+{
+  QueryResult expected_result;
+  boost::posix_time::ptime start_time;
+  boost::posix_time::ptime end_time;
+  boost::shared_ptr<DBThreadState> thr_state;
+
+  assert(t);
+
+  thr_state= t->get_state();
+
+  assert(thr_state.get());
+
+  ConnectionState &state= (ConnectionState &)*thr_state;
+
+  LastExecutedQueryInfo &last_query_info= state.last_executed_query_info;
+
+  if (g_tcpdump_mode == TCPDUMP_MODE_ACCURATE &&
+      last_query_info.end_timestamp.tv_sec != 0 &&
+      last_query_info.end_timestamp.tv_usec != 0 &&
+      last_query_info.end_pcap_timestamp.tv_sec != 0 &&
+      last_query_info.end_pcap_timestamp.tv_usec != 0)
+  {
+    timeval current_timestamp;
+    timeval pause_time;
+    timeval pcap_pause_time;
+
+    gettimeofday(&current_timestamp, NULL);
+    timersub(&current_timestamp, &last_query_info.end_timestamp, &pause_time);
+
+    timersub(&pcap_timestamp,
+             &last_query_info.end_pcap_timestamp,
+             &pcap_pause_time);
+
+    if (timercmp(&pause_time, &pcap_pause_time, <))
+    {
+      timeval time_to_sleep;
+      timersub(&pcap_pause_time,
+               &pause_time,
+               &time_to_sleep);
+      usleep(time_to_sleep.tv_sec*1000000 + time_to_sleep.tv_usec);
+    }
+  }
+
+  last_query_info.result.clear();
+
+  start_time= boost::posix_time::microsec_clock::universal_time();
+  t->execute_query(query, &last_query_info.result, expected_result);
+  end_time= boost::posix_time::microsec_clock::universal_time();
+
+  boost::posix_time::time_period duration(start_time, end_time);
+  last_query_info.result.setDuration(duration.length());
+  last_query_info.begin_pcap_timestamp= pcap_timestamp;
+  last_query_info.query= query;
+}
+
+void
+TcpdumpResultEntry::execute(DBThread *t)
+{
+  timeval query_execution_time;
+  boost::shared_ptr<DBThreadState> thr_state;
+  
+  assert(t);
+  
+  thr_state= t->get_state();
+
+  assert(thr_state.get());
+
+
+  ConnectionState &state= (ConnectionState &)*thr_state;
+  LastExecutedQueryInfo &last_query_info= state.last_executed_query_info;
+
+  timersub(&pcap_timestamp,
+           &last_query_info.begin_pcap_timestamp,
+           &query_execution_time);
+
+  expected_result.setDuration(
+    boost::posix_time::microseconds(
+      query_execution_time.tv_usec +
+      query_execution_time.tv_sec*1000000));
+
+  if (g_tcpdump_mode == TCPDUMP_MODE_ACCURATE &&
+      (expected_result.getDuration() > last_query_info.result.getDuration()))
+  {
+    boost::posix_time::time_duration us_sleep_time=
+      expected_result.getDuration() - last_query_info.result.getDuration();
+
+    usleep(us_sleep_time.total_microseconds());
+  }
+
+  gettimeofday(&last_query_info.end_timestamp, NULL);
+  last_query_info.end_pcap_timestamp= pcap_timestamp;
+
+  BOOST_FOREACH(const percona_playback::PluginRegistry::ReportPluginPair pp,
+                percona_playback::PluginRegistry::singleton().report_plugins)
+  {
+    pp.second->query_execution(getThreadId(),
+                               last_query_info.query,
+                               expected_result,
+                               last_query_info.result);
+  }
+}
---
+++ percona_playback/tcpdump/tcpdump_query_entries.h
@@ -0,0 +1,73 @@
+/* BEGIN LICENSE
+ * Copyright (C) 2011 Percona Inc.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * END LICENSE */
+
+#ifndef PERCONA_PLAYBACK_TCPDUMP_QUERY_ENTRIES_H
+#define PERCONA_PLAYBACK_TCPDUMP_QUERY_ENTRIES_H
+
+#include "percona_playback/query_entry.h"
+#include "percona_playback/query_result.h"
+#include "connection_state.h"
+
+#include <sys/time.h>
+
+class TcpdumpQueryEntry : public QueryEntry
+{
+  timeval       pcap_timestamp;
+  std::string   query;
+
+public:
+
+  TcpdumpQueryEntry()
+  {
+    pcap_timestamp.tv_sec= pcap_timestamp.tv_usec= 0;
+  }
+
+  TcpdumpQueryEntry(const timeval       &pcap_timestamp,
+                    const std::string   &query,
+                    const AddrPort      &addr_port) :
+    QueryEntry(addr_port.ThreadId(), false),
+    pcap_timestamp(pcap_timestamp),
+    query(query)
+  {}
+
+  bool is_quit() { return false; }
+
+  void execute(DBThread *t);
+};
+
+class TcpdumpResultEntry : public QueryEntry
+{
+
+  timeval       pcap_timestamp;
+  QueryResult   expected_result;
+
+public:
+  TcpdumpResultEntry() {}
+  TcpdumpResultEntry(const AddrPort     &addr_port,
+                     const timeval      &pcap_timestamp,
+                     const QueryResult  &expected_result) :
+    QueryEntry(addr_port.ThreadId(), false),
+    pcap_timestamp(pcap_timestamp),
+    expected_result(expected_result)
+  {}
+
+  bool is_quit() { return false; }
+
+  void execute(DBThread *t);
+
+};
+
+#endif// PERCONA_PLAYBACK_TCPDUMP_QUERY_ENTRIES_H
+
