e78a0b5fcfc60e19368767ab9e5925368b528e1f
Stewart Smith <stewart@flamingspork.com>
2012-06-21 17:53:19 +1000

    use autoreconf instead of manually calling a bunch of autotools foo.

--- config/autorun.sh
+++ config/autorun.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-#
-#  Copyright (C) 2006 Jan Kneschke
-#  Copyright (C) 2009 Sun Microsystems, Inc.
+#  Copyright (C) 2011 Brian Aker
 #  All rights reserved.
 # 
 #  Redistribution and use in source and binary forms, with or without
@@ -26,20 +24,8 @@
 #  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 #  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 #  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# Run this to generate all the initial makefiles, etc.
 
 die() { echo "$@"; exit 1; }
-
-# --force means overwrite ltmain.sh script if it already exists 
-LIBTOOLIZE_FLAGS=" --automake --copy --force"
-# --add-missing instructs automake to install missing auxiliary files
-# and --force to overwrite them if they already exist
-AUTOMAKE_FLAGS="--add-missing --copy --force --foreign"
-ACLOCAL_FLAGS="-I m4"
-
-ARGV0=$0
-ARGS="$@"
 
 run() {
 	echo "$ARGV0: running \`$@' $ARGS"
@@ -63,64 +49,18 @@ locate_binary() {
 }
 
 
+AUTORECONF_FLAGS=" --install --force --verbose -Wall"
+
 if test -f config/pre_hook.sh
 then
   . config/pre_hook.sh
 fi
 
-# Try to detect the supported binaries if the user didn't
-# override that by pushing the environment variable
-if test x$LIBTOOLIZE = x; then
-  LIBTOOLIZE=`locate_binary glibtoolize libtoolize-1.5 libtoolize`
-  if test x$LIBTOOLIZE = x; then
-    die "Did not find a supported libtoolize"
-  fi
-fi
-
-if test x$ACLOCAL = x; then
-  ACLOCAL=`locate_binary aclocal-1.11 aclocal-1.10 aclocal-1.9 aclocal19 aclocal`
-  if test x$ACLOCAL = x; then
-    die "Did not find a supported aclocal"
-  fi
-fi
-
-if test x$AUTOMAKE = x; then
-  AUTOMAKE=`locate_binary automake-1.11 automake-1.10 automake-1.9 automake19 automake`
-  if test x$AUTOMAKE = x; then
-    die "Did not find a supported automake"
-  fi
-fi
-
-if test x$AUTOCONF = x; then
-  AUTOCONF=`locate_binary autoconf-2.59 autoconf259 autoconf`
-  if test x$AUTOCONF = x; then
+if test x$AUTORECONF = x; then
+  AUTORECONF=`locate_binary autoreconf`
+  if test x$AUTORECONF = x; then
     die "Did not find a supported autoconf"
   fi
 fi
 
-if test x$AUTOHEADER = x; then
-  AUTOHEADER=`locate_binary autoheader-2.59 autoheader259 autoheader`
-  if test x$AUTOHEADER = x; then
-    die "Did not find a supported autoheader"
-  fi
-fi
-
-run $LIBTOOLIZE $LIBTOOLIZE_FLAGS || die "Can't execute libtoolize"
-run $ACLOCAL $ACLOCAL_FLAGS || die "Can't execute aclocal"
-run $AUTOHEADER || die "Can't execute autoheader"
-run $AUTOMAKE $AUTOMAKE_FLAGS  || die "Can't execute automake"
-run $AUTOCONF || die "Can't execute autoconf"
-
-if test -f config/post_hook.sh
-then
-  . config/post_hook.sh
-fi
-
-echo "---"
-echo "Configured with the following tools:"
-echo "  * `$LIBTOOLIZE --version | head -1`"
-echo "  * `$ACLOCAL --version | head -1`"
-echo "  * `$AUTOHEADER --version | head -1`"
-echo "  * `$AUTOMAKE --version | head -1`"
-echo "  * `$AUTOCONF --version | head -1`"
-echo "---"
+run $AUTORECONF $AUTORECONF_FLAGS || die "Can't execute autoreconf"
