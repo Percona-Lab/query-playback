17ad40babe5ee142b72f9ff2c928dbccfb333033
Stewart Smith <stewart@flamingspork.com>
2013-06-07 17:07:06 +1000

    fix up some left over GPLv3 notices which should have been switched to GPLv2

--- percona_playback/test/basic-multiline.cc
+++ percona_playback/test/basic-multiline.cc
@@ -1,7 +1,7 @@
 /* BEGIN LICENSE
- * Copyright (C) 2013 Percona Ireland Ltd.
+ * Copyright (C) 2011-2013 Percona Ireland Ltd.
  * This program is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 3, as published
+ * under the terms of the GNU General Public License version 2, as published
  * by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but
--- percona_playback/test/basic-stdin.cc
+++ percona_playback/test/basic-stdin.cc
@@ -1,9 +1,8 @@
 /* BEGIN LICENSE
  * Copyright (C) 2011-2013 Percona Ireland Ltd.
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License version 3, as published by the
- * Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2, as published
+ * by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranties of
