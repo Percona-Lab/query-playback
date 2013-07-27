da36719b13163a1aa4bc05e03575e7fd01c6afd1
Stewart Smith <stewart@flamingspork.com>
2013-07-27 12:46:45 +1000

    tcpdump/pcap_packets_parser.cc needs config.h included first

--- percona_playback/tcpdump/pcap_packets_parser.cc
+++ percona_playback/tcpdump/pcap_packets_parser.cc
@@ -12,6 +12,8 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  * END LICENSE */
+
+#include <config.h>
 #include "pcap_packets_parser.h"
 #include "sniff_headers.h"
 #include "tcpdump.h"
